697528fd433b66d270d9512224e73eb30844799b
Samuel Phillips <samphillips1879@gmail.com>
2017-01-19 14:42:03 -0600

    adminCustomization

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -14,7 +14,11 @@ class Question(models.Model):
     	return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -22,4 +26,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-    	return self.question_text
+    	return self.choice_text
